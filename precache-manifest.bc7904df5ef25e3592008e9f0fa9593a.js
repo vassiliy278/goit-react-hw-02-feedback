22da7eabdd986c85159012edabdcb8c0e0599160
Vassiliy278 <trafaret2009@gmail.com>
2020-07-10 12:19:44 +0300

    Updates

---
+++ precache-manifest.bc7904df5ef25e3592008e9f0fa9593a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b73a24091ae889dc499460852cfca7",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "d9859ca55ecd626d7733",
+    "url": "/goit-react-hw-02-feedback/static/css/main.4c25a5aa.chunk.css"
+  },
+  {
+    "revision": "96858b702c9ef6cf9eca",
+    "url": "/goit-react-hw-02-feedback/static/js/2.4522d62d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.4522d62d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9859ca55ecd626d7733",
+    "url": "/goit-react-hw-02-feedback/static/js/main.45ae1f86.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
